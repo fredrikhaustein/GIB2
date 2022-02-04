95280f9f9eacf27d2868f10138dae39a90387f6a
Sigurd Skatvedt <s.skatvedt@gmail.com>
2022-02-04 21:24:32 +0100

    tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,11 @@
-function HomePage() {
-        return <div>Welcome to Next.js!</div>;
-}
+import React from "react";
 
-export default HomePage;
+export default function index() {
+        return (
+                <div className="bg-gray-100">
+                        <h1 className="text-3xl font-bold underline">
+                                Hello world
+                        </h1>
+                </div>
+        );
+}
