a564e237493b7db644aa052116b3b55869593ca9
Sigurd Skatvedt <s.skatvedt@gmail.com>
2022-02-17 11:37:19 +0100

    drowdown-menu with mapchange

--- components/map.tsx
+++ components/map.tsx
@@ -1,8 +1,19 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {
+        MapContainer,
+        Marker,
+        Popup,
+        TileLayer,
+        useMap,
+        useMapEvent,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { Mountain } from "../src/types";
+import { useCallback, useEffect, useRef, useState } from "react";
+import MapChangeButton from "./mapChangeButton";
+import { latLng } from "leaflet";
+import React from "react";
 
-const Map = ({ mapData }) => {
+const MountainMap = ({ mapData }) => {
         let mapDataArray: Array<Mountain> = mapData.mountains.map(
                 (mountain: Mountain) => ({
                         ogc_fid: mountain.ogc_fid,
@@ -13,13 +24,23 @@ const Map = ({ mapData }) => {
                         wkb_geometry: mountain.wkb_geometry,
                 })
         );
-        const mountain: Mountain = mapDataArray.find(
-                (e) => e.navn == "Snøhetta"
-        );
-        console.log(mountain);
-        //let mountain = mapDataArray.key(1);
-        //console.log(mountain.lat);
-        //console.log(mountain.lon);
+
+        const findMountain = (fid: number) => {
+                return mapDataArray.find((e) => e.ogc_fid == fid);
+        };
+
+        const mountain: Mountain = findMountain(1);
+        const newMountain: Mountain = findMountain(2);
+
+        const [map, setMap] = useState(null);
+        async function handleMapButton(newMountain: Mountain) {
+                await map.panTo(latLng(newMountain.lat, newMountain.lon));
+        }
+
+        const handleMountainChange = (e) => {
+                const newMountain = findMountain(e.target.value);
+                handleMapButton(newMountain);
+        };
 
         return (
                 <div>
@@ -28,11 +49,15 @@ const Map = ({ mapData }) => {
                                 zoom={13}
                                 scrollWheelZoom={false}
                                 style={{ height: 400, width: "100%" }}
+                                whenCreated={setMap}
                         >
                                 <TileLayer
                                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                                 />
+                                {/*
+                                 *<UpdateMapCenter newMountain={newMountain} />
+                                 */}
                                 <Marker position={[51.505, -0.09]}>
                                         <Popup>
                                                 A pretty CSS3 popup. <br />{" "}
@@ -40,8 +65,18 @@ const Map = ({ mapData }) => {
                                         </Popup>
                                 </Marker>
                         </MapContainer>
+                        <select onChange={handleMountainChange}>
+                                <option value="1">
+                                        {findMountain(1).navn}
+                                </option>
+                                {mapDataArray.map((mountain: Mountain) => (
+                                        <option value={mountain.ogc_fid}>
+                                                {mountain.navn}
+                                        </option>
+                                ))}
+                        </select>
                 </div>
         );
 };
 
-export default Map;
+export default MountainMap;
