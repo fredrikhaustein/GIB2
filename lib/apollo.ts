0932b2530aba761059e2e8bd4d4403dc33f8799f
Sigurd Skatvedt <s.skatvedt@gmail.com>
2022-04-30 11:44:03 +0200

    remote graphql link fix

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const apolloClient = new ApolloClient({
-        uri: "http://localhost:3000/api/graphql",
+        uri: "gib2.vercel.app/api/graphql",
         cache: new InMemoryCache(),
 });
 
