ea286eac3909b4e285dda7bddc22037df90fe4da
Sigurd Skatvedt <s.skatvedt@gmail.com>
2022-05-02 17:53:44 +0200

    link fix

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -2,8 +2,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const apolloClient = new ApolloClient({
-  uri: "https://gib2.vercel.app/api/graphql",
-  cache: new InMemoryCache(),
+        uri: "https://localhost:3000/api/graphql",
+        cache: new InMemoryCache(),
 });
 
 export default apolloClient;
